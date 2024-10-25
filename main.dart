614da890b35a68cdc9836421c482763564aaec61
Miguel Estivalet Pinto <miguelestivaletpinto@gmail.com>
2024-10-25 16:31:30 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,68 +1,51 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
+// O método main é o ponto de entrada do aplicativo.
 void main() {
   runApp(MyApp());
 }
 
+// Classe principal do aplicativo, que define o tema e a página inicial.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: UFSCaducasApp(),
       theme: ThemeData(
-        fontFamily: 'Roboto',
+        fontFamily: 'Roboto', // Define a fonte padrão do aplicativo.
       ),
     );
   }
 }
 
+// Definição do estado inicial do aplicativo UFSCaducasApp.
 class UFSCaducasApp extends StatefulWidget {
   @override
   _UFSCaducasAppState createState() => _UFSCaducasAppState();
 }
 
+// Estado da página principal do aplicativo.
 class _UFSCaducasAppState extends State<UFSCaducasApp> {
-  double _fontSize = 16.0; // Tamanho inicial da fonte
+  double _fontSize = 16.0; // Tamanho inicial da fonte.
+  bool flag = true; // Flag de controle para uma futura lógica.
 
-  @override
-  void initState() {
-    super.initState();
-    _loadFontSize(); // Carrega o tamanho da fonte ao iniciar o app
-  }
-
-  Future<void> _loadFontSize() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _fontSize = prefs.getDouble('fontSize') ?? 16.0;
-    });
-  }
-
-  Future<void> _saveFontSize() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setDouble('fontSize', _fontSize);
-  }
-
+  // Função que aumenta o tamanho da fonte.
   void _increaseFontSize() {
     setState(() {
       _fontSize += 2;
     });
-    _saveFontSize();
   }
 
+  // Função que diminui o tamanho da fonte, mas evita que fique muito pequena.
   void _decreaseFontSize() {
     setState(() {
       _fontSize = _fontSize > 10 ? _fontSize - 2 : _fontSize;
     });
-    _saveFontSize();
   }
 
+  // Função que ajusta o tamanho do ícone com base no tamanho da fonte.
   double _calculateIconSize(double fontSize) {
-    if (fontSize > 22) {
-      return 40;
-    } else {
-      return 50;
-    }
+    return fontSize > 22 ? 40 : 50;
   }
 
   @override
@@ -79,16 +62,17 @@ class _UFSCaducasAppState extends State<UFSCaducasApp> {
             ),
           ),
         ),
-        backgroundColor: Color(0xFF6C5481),
-        elevation: 0,
+        backgroundColor: Color(0xFF6C5481), // Define a cor da AppBar.
+        elevation: 0, // Remove a sombra da AppBar.
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: GridView.count(
-          crossAxisCount: 2,
+          crossAxisCount: 2, // Define duas colunas para o GridView.
           crossAxisSpacing: 10,
           mainAxisSpacing: 10,
           children: [
+            // Cada um dos itens do GridView, com título, ícone e comportamento ao clicar.
             buildGridItem(
                 'Quando fazer um PIX?', Icons.payment, _fontSize, context, 0),
             buildGridItem('Como fazer uma senha?', Icons.lock_outline,
@@ -105,21 +89,21 @@ class _UFSCaducasAppState extends State<UFSCaducasApp> {
         ),
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Color(0xFF6C5481),
+        color: Color(0xFF6C5481), // Define a cor do BottomAppBar.
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             IconButton(
               icon: Icon(Icons.elderly, size: 40, color: Colors.white),
-              onPressed: () {},
+              onPressed: () {}, // Ação ao clicar no ícone.
             ),
             IconButton(
               icon: Icon(Icons.text_increase, size: 40, color: Colors.white),
-              onPressed: _increaseFontSize, // Aumenta o texto
+              onPressed: _increaseFontSize, // Aumenta o texto.
             ),
             IconButton(
               icon: Icon(Icons.text_decrease, size: 40, color: Colors.white),
-              onPressed: _decreaseFontSize, // Diminui o texto
+              onPressed: _decreaseFontSize, // Diminui o texto.
             ),
           ],
         ),
@@ -127,23 +111,26 @@ class _UFSCaducasAppState extends State<UFSCaducasApp> {
     );
   }
 
+  // Função que cria cada item do grid com título, ícone e funcionalidade de navegação.
   Widget buildGridItem(String title, IconData icon, double fontSize,
       BuildContext context, int index) {
-    double iconSize = _calculateIconSize(fontSize);
+    double iconSize = _calculateIconSize(fontSize); // Define o tamanho do ícone.
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => DetailScreen(
-                title: title, idNumber: index, fontSize: fontSize),
+                title: title,
+                idNumber: index,
+                fontSize: fontSize), // Abre a tela de detalhes ao clicar.
           ),
         );
       },
       child: Container(
         decoration: BoxDecoration(
-          color: Color(0xFF8A7194),
-          borderRadius: BorderRadius.circular(12),
+          color: Color(0xFF8A7194), // Cor de fundo do item.
+          borderRadius: BorderRadius.circular(12), // Borda arredondada.
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
@@ -154,9 +141,9 @@ class _UFSCaducasAppState extends State<UFSCaducasApp> {
               SizedBox(height: 10),
               Text(
                 title,
-                textAlign: TextAlign.center,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center, // Centraliza o texto.
+                maxLines: 2, // Limita o texto a duas linhas.
+                overflow: TextOverflow.ellipsis, // Adiciona "..." caso ultrapasse.
                 style: TextStyle(
                     color: Colors.white,
                     fontSize: fontSize,
@@ -170,6 +157,7 @@ class _UFSCaducasAppState extends State<UFSCaducasApp> {
   }
 }
 
+// Tela de detalhes para exibir informações mais completas.
 class DetailScreen extends StatefulWidget {
   final String title;
   final double fontSize;
@@ -188,12 +176,12 @@ class _DetailScreenState extends State<DetailScreen> {
   @override
   void initState() {
     super.initState();
-    _fontSize = widget.fontSize;
+    _fontSize = widget.fontSize; // Define o tamanho inicial da fonte da tela.
   }
 
   void _increaseFontSize() {
     setState(() {
-      _fontSize += 2;
+      _fontSize += 2; // Aumenta o tamanho da fonte.
     });
   }
 
@@ -204,11 +192,7 @@ class _DetailScreenState extends State<DetailScreen> {
   }
 
   double _calculateIconSize(double fontSize) {
-    if (fontSize > 22) {
-      return 40;
-    } else {
-      return 50;
-    }
+    return fontSize > 22 ? 40 : 50; // Ajusta o tamanho do ícone.
   }
 
   @override
@@ -228,7 +212,7 @@ class _DetailScreenState extends State<DetailScreen> {
         child: ListView(
           children: [
             Text(
-              detalhe[widget.idNumber],
+              detalhe[widget.idNumber], // Exibe o texto correspondente ao item.
               style: TextStyle(fontSize: _fontSize),
             ),
           ],
@@ -241,11 +225,11 @@ class _DetailScreenState extends State<DetailScreen> {
           children: [
             IconButton(
               icon: Icon(Icons.text_increase, size: 40, color: Colors.white),
-              onPressed: _increaseFontSize,
+              onPressed: _increaseFontSize, // Aumenta o texto.
             ),
             IconButton(
               icon: Icon(Icons.text_decrease, size: 40, color: Colors.white),
-              onPressed: _decreaseFontSize,
+              onPressed: _decreaseFontSize, // Diminui o texto.
             ),
           ],
         ),
@@ -254,11 +238,12 @@ class _DetailScreenState extends State<DetailScreen> {
   }
 }
 
+// Lista com o conteúdo dos detalhes de cada item.
 List<String> detalhe = [
-  'O Pix é uma forma rápida e prática de transferir dinheiro...',
-  'Criar uma boa senha exige atenção...',
-  'Ao usar a internet, é comum receber e-mails...',
-  'Fazer compras pela internet exige muito cuidado...',
-  'Atualmente, existem muitos programas (ou "aplicativos")...',
+  ' O Pix é uma forma rápida e prática de transferir dinheiro...',
+  ' Criar uma boa senha exige atenção...',
+  ' Ao usar a internet, é comum receber e-mails de remetentes desconhecidos...',
+  ' Fazer compras pela internet exige muito cuidado...',
+  ' Atualmente, existem muitos programas (ou "aplicativos") disponíveis...',
   'Ao receber chamadas: verifique se o número é desconhecido...',
 ];
